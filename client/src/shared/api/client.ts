ccfcb5eacf5f484cf9afbdae40b83a3e25532be8
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2026-03-20 15:09:57 +0100

    api endpoint root modified for client

--- client/src/shared/api/client.ts
+++ client/src/shared/api/client.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { VITE_API_URL } from "../constants/API_URL";
 
 let accessToken = localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token") as string) : null;
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     "Authorization": `Bearer ${accessToken?.accessToken || ""}`,
---
+++ client/src/shared/constants/API_URL.ts
@@ -0,0 +1,2 @@
+//  VITE_API_URL="http://localhost:5000"
+export const VITE_API_URL="https://billing-invoice-ai-software.onrender.com/"
